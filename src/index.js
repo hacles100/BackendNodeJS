4310487d5ceb615f8ad108d5d576a212e6bd36fe
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2023-01-08 23:27:56 -0100

    create methods

--- src/index.js
+++ src/index.js
@@ -3,9 +3,46 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
 
-    return response.send('Ola Mundo')
+app.get('/projects', (request, response) => {
+
+    return response.json([
+        'project 1',
+        'project 2'
+    ]);
+});
+
+
+app.post('/projects', (request, response) => {
+
+    return response.json([
+        'project 1',
+        'project 2',
+        'project 3'
+    ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+
+    return response.json([
+        'project 3',
+        'project 2',
+        'project 1'
+    ]);
+});
+
+
+
+app.delete('/projects/:id', (request, response) => {
+
+    return response.json([
+        'project 1',
+        'project 2'
+    ]);
+});
+
+
+app.listen(3333, () => {
+    console.log('🚀 Backend Started!')
 })
-
-app.listen(3333)
